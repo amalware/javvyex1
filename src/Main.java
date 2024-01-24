0474bd960edd99b11986a34e01879cb5b476ffde
amalware <a.msuleiman@outlook.com>
2024-01-24 10:43:34 -0500

    Amal Suleiman, Ex 1

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,23 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+//Amal Suleiman
+//Jav EX1
+//January 24th 2024
+
 public class Main {
     public static void main(String[] args) {
         int dailyHighs[] = {32, 25, 27, 40, 45};
-        double total = 0;
 
-    for( int i=0; i<dailyHighs.length; i++){
-        total = total + dailyHighs[i];
-    }
+        double total = 0; //Initialize total variable
 
-        System.out.print("City: Columbus ");
-        System.out.print("Zip Code: 43125 ");
+        for( int i=0; i<dailyHighs.length; i++){ //Start at index 0, for the length of the array...
+            total = total + dailyHighs[i]; //Add index int value to each total
+        }
+        //Note: I did not do System.out.println() because I wanted this all on one line!
+        System.out.print("City: Columbus "); //City
+        System.out.print("Zip Code: 43125 "); //Zip code
         System.out.print("Average High Temperature: ");
-        System.out.print(total/dailyHighs.length);
+        System.out.print(total/dailyHighs.length); //Find average (add up total, divide by length) and print (yes, in one line)
 
-
+        //When computing the average high temp, the value was stored as a double. The average had a decimal (33.8). Intergers do NOT include decimals. Therefore, the computed average with either data types ends up as a quote.
     }
 }
 
